a5fe3b4ff7faeb1b2a4fb37d830c0181bf59eccd
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2013-11-19 16:21:45 +0100

    Bug: correctly close the activity when there is an empty note.

--- src/me/sivieri/dimatodos/NoteActivity.java
+++ src/me/sivieri/dimatodos/NoteActivity.java
@@ -95,6 +95,7 @@ public class NoteActivity extends Activity {
 			if (title.length() == 0 && content.length() == 0) {
 				if (this.uri == null) {
 					Toast.makeText(getApplicationContext(), getString(R.string.note_discarded), Toast.LENGTH_SHORT).show();
+					super.finish();
 				}
 				else {
 					AlertDialog.Builder builder = new AlertDialog.Builder(this);
